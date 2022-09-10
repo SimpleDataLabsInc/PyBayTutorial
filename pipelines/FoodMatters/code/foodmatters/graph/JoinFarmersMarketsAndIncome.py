08fb11489d85afab3ad5a7ba2159c06a2eb0eb55
anya.bida1@gmail.com <anya.bida1@gmail.com>
2022-09-10 18:04:03 +0000

    commit

--- pipelines/FoodMatters/code/foodmatters/graph/JoinFarmersMarketsAndIncome.py
+++ pipelines/FoodMatters/code/foodmatters/graph/JoinFarmersMarketsAndIncome.py
@@ -7,5 +7,4 @@ from foodmatters.udfs.UDFs import *
 def JoinFarmersMarketsAndIncome(spark: SparkSession, farmers_markets: DataFrame, tax_data: DataFrame, ) -> DataFrame:
     return farmers_markets\
         .alias("farmers_markets")\
-        .hint("shuffle_hash")\
         .join(tax_data.alias("tax_data"), (col("farmers_markets.zip") == col("tax_data.zipcode")), "right_outer")
